6464d5c626a396b75e1be6ad0687718876e23ed9
Vickey <vickey21299@iiitd.ac.in>
2023-06-13 15:16:56 +0530

    first commit

---
+++ navchenta_welcome/app/src/main/java/com/example/navchenta_welcome/Sing_up.kt
@@ -0,0 +1,11 @@
+package com.example.navchenta_welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sing_up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+    }
+}
---
+++ navchenta_welcome/app/src/main/java/com/example/navchenta_welcome/Sing_up1.kt
@@ -0,0 +1,11 @@
+package com.example.navchenta_welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sing_up1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up1)
+    }
+}
