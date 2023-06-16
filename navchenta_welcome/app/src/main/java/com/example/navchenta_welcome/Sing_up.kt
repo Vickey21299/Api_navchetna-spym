d6d01fcda9bdbba3bf1456842c464f078d8c54ff
Vickey <vickey21299@iiitd.ac.in>
2023-06-16 23:12:36 +0530

    third commit

--- navchenta_welcome/app/src/main/java/com/example/navchenta_welcome/Sing_up.kt
+++ navchenta_welcome/app/src/main/java/com/example/navchenta_welcome/Sing_up.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class Sing_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_up)
     }
