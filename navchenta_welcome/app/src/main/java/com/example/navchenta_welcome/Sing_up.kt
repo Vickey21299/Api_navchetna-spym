457a68d5b5e95a7c2763a42cf0c4d30bd047b879
Vickey <vickey21299@iiitd.ac.in>
2023-06-17 09:20:49 +0530

    fouth commit

--- navchenta_welcome/app/src/main/java/com/example/navchenta_welcome/Sing_up.kt
+++ navchenta_welcome/app/src/main/java/com/example/navchenta_welcome/Sing_up.kt
@@ -2,11 +2,48 @@ package com.example.navchenta_welcome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Toast
+import com.example.navchenta_welcome.databinding.ActivitySingUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Sing_up : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    private lateinit var binding: ActivitySingUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sing_up)
+
+        binding = ActivitySingUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.goToLogin.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        }
+        binding.singup.setOnClickListener {
+            val email = binding.yourEmail.text.toString()
+            val pass = binding.setPassward.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, login::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
--- navchenta_welcome/app/src/main/java/com/example/navchenta_welcome/login.kt
+++ navchenta_welcome/app/src/main/java/com/example/navchenta_welcome/login.kt
@@ -2,11 +2,46 @@ package com.example.navchenta_welcome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Toast
+import com.example.navchenta_welcome.databinding.ActivityLoginBinding
+import com.example.navchenta_welcome.databinding.ActivitySingUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.goToSingup.setOnClickListener {
+            val intent = Intent(this, Sing_up::class.java)
+            startActivity(intent)
+        }
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginEmail.text.toString()
+            val pass = binding.loginPassward.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, Sing_up1::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
