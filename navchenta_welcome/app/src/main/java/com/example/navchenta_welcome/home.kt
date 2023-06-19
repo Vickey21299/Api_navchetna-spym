62df1a75a70ef226fabfeda0615cfe3702ed9df3
Vickey <vickey21299@iiitd.ac.in>
2023-06-20 01:05:33 +0530

    up to bottom bar

--- navchenta_welcome/app/src/main/java/com/example/navchenta_welcome/home.kt
+++ navchenta_welcome/app/src/main/java/com/example/navchenta_welcome/home.kt
@@ -1,11 +1,19 @@
 package com.example.navchenta_welcome
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        var Navcontroller : NavController = findNavController(R.id.fragmentContainerView)
+        var bottomNav : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNav.setupWithNavController(Navcontroller)
     }
 }
